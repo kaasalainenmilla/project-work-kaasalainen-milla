dc8e0cb67f0acc7b47b24b5db2871002944a92d2
kaasalainenmilla <mkaasa96@gmail.com>
2023-01-29 22:54:02 +0200

    Program Shell

---
+++ Main.java
@@ -0,0 +1,124 @@
+import util.*;
+import java.io.*; 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        /* Initializing file reader */
+        Console c = System.console(); 
+        System.out.println("Input 1, if you want to add a new contact");
+        System.out.println("Input 2 to view contacts");
+        System.out.println("Input 3 to remove contact");
+        System.out.println("Input 4 to update contact");
+        int input = Integer.parseInt(c.readLine());
+        int id = lines() + 1;
+        if(input == 1) {
+            newContact(id);
+        }
+        if(input == 2) {
+            String line = null;
+            try (BufferedReader file = new BufferedReader(new FileReader("contacts.txt"))){
+                while((line = file.readLine()) != null) {
+                    System.out.println(line);
+                }
+                file.close();
+            }
+            catch(IOException e)
+            {
+                System.out.println("Error");
+            }
+        }
+        if(input == 3) {
+            System.out.println("Which contact id do you wish to delete?");
+            String number = c.readLine();
+            remove(number);
+        }
+        if(input == 4) {
+            System.out.println("Which contact id do you wish to update?");
+            String number = c.readLine();
+            update(number);
+        }
+    }
+    public static int lines() {
+        /* Getting id from number of lines */
+        int lines = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("contacts.txt"))) {
+            while (reader.readLine() != null) lines++;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+    public static void newContact(int id) {
+        /* Creates a new contact */
+        Console c = System.console(); 
+        System.out.println("Please input contact first name");
+        String fName = c.readLine();
+        System.out.println("Please input contact last name");
+        String lName = c.readLine();
+        Contact input = new Contact(id, fName, lName);
+        System.out.println("New contact " + fName + " succesfully created");  
+    }
+    public static void remove(String number) {
+        try {
+            File inputFile = new File("contacts.txt");
+            File tempFile = new File("contacts2.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String lineToRemove = number;
+            String currentLine;
+            while((currentLine = reader.readLine()) != null) {
+                String trimmedLine = currentLine.trim();
+                if(trimmedLine.startsWith(lineToRemove)){
+                 continue;
+                } else {
+                    writer.write(currentLine + System.getProperty("line.separator"));
+                }
+            }
+            writer.close(); 
+            reader.close(); 
+            inputFile.delete();
+            boolean flag = tempFile.renameTo(inputFile);
+            if (flag == true) {
+                System.out.println("Contact deleted");
+            } else {
+                System.out.println("Operation Failed");
+            }
+        } catch (IOException e) {
+                e.printStackTrace(); 
+        }
+    }
+    public static void update(String number) {
+        try {
+            File inputFile = new File("contacts.txt");
+            File tempFile = new File("contacts2.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String lineToUpdate = number;
+            String currentLine;
+            while((currentLine = reader.readLine()) != null) {
+                String trimmedLine = currentLine.trim();
+                if(trimmedLine.startsWith(lineToUpdate)){
+                 continue;
+                } else {
+                    writer.write(currentLine + System.getProperty("line.separator"));
+                }
+            }
+            writer.close(); 
+            reader.close(); 
+            inputFile.delete();
+            boolean flag = tempFile.renameTo(inputFile);
+            if (flag == true) {
+                System.out.println("Please input new contact info");
+            } else {
+                System.out.println("Operation Failed");
+            }
+        } catch (IOException e) {
+                e.printStackTrace(); 
+        }
+        int id = Integer.parseInt(number);
+        newContact(id);
+    }
+}
+
+
---
+++ util.java
@@ -0,0 +1,66 @@
+import java.io.*; 
+
+class Contact {
+    /* Class containing all contact information. */
+    /* Initializing file reader */
+    BufferedWriter output;
+    private String id;
+    private String fName;
+    private String lName;
+    private String phone;
+    private String address;
+    private String email;
+
+    public void setId(String userId) {
+        this.id = userId;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setFname(String userFname) {
+        this.fName = userFname;
+    }
+    public String getFname() {
+        return fName;
+    }
+    public void setLname(String userLname) {
+        this.lName = userLname;
+    }
+    public String getLname() {
+        return lName;
+    }
+    public void setPhone(String userPhone) {
+        this.phone = userPhone;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setAddress(String userAddress) {
+        this.address = userAddress;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setEmail(String userEmail) {
+        this.email = userEmail;
+    }
+    public String getEmail() {
+        return email;
+    }
+    int lastId = 1;
+    /* Creating new contact */
+    public Contact(int id, String fName, String lName) {
+        setId(Integer.toString(id));
+        setFname(fName);
+        setLname(lName);
+        try {
+            output = new BufferedWriter(new FileWriter("contacts.txt", true));
+            output.append(Integer.toString(id) + ", " + fName + ", " + lName);
+            output.newLine();
+            output.close();   
+        }
+        catch (IOException e){
+            System.out.println("Error");
+        }
+    }
+}
